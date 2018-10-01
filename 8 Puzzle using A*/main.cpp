8ae4e7533f81fd381f96d987de3aeb378eb3b837
Rushikesh bhosle <rushisbhosle155@gmail.com>
2018-10-02 01:47:42 +0530

    fully working 8 puzzle using A*

--- 8 Puzzle using A*/main.cpp
+++ 8 Puzzle using A*/main.cpp
@@ -1,34 +1,103 @@
 #include <iostream>
 using namespace std;
 
+class node{
+public:
+    char grid[3][3];
+    int path_cost;
+    int total_cost;
+    int visited;
+    char moves[100];
+    node* next;
+    node(){
+        path_cost=-1;
+        total_cost=-1;
+        next=NULL;
+        visited=0;
+    }
+};
+
 class Puzzle{
 private:
+    node* top;
     char initial_grid[3][3];
     char goal_grid[3][3];
-    char curr_grid[3][3];
-    int no_of_steps;
-    char moves[100];
 public:
     Puzzle();
     int huristic_manhattan(char current_grid[3][3]);
-    char select_next_node();
+    void add_neighbours(node* temp);
     void get();
     void solve();
-    void print_result();
+    void print_result(node* res);
     void print_grid(char g[3][3]);
-    void print_steps();
+    void print_steps(node* res);
     void swap(char& c1,char&c2);
+    int not_on_que(char temp[3][3]);
+    void add_to_que(char grid[3][3],node* pre,char move);
+    node* select_best_node();
 
 };
 Puzzle::Puzzle(){
+    top=NULL;
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
             initial_grid[i][j]='-';
             goal_grid[i][j]='-';
         }
     }
-    no_of_steps=0;
 }
+
+int Puzzle::not_on_que(char temp[3][3]){
+    node* curr=top;
+    while(curr!=NULL){
+        int flag=0;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(temp[i][j]!=curr->grid[i][j]){
+                    flag=1;
+                }
+            }
+        }
+        if(flag!=1){
+            return 0;
+        }
+        curr=curr->next;
+    }
+    return 1;
+}
+
+void Puzzle::add_to_que(char grid[3][3], node *pre, char move) {
+    node* curr=new node();
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            curr->grid[i][j]=grid[i][j];
+        }
+    }
+    curr->next=top;
+    top=curr;
+    strcpy(curr->moves,pre->moves);
+    curr->path_cost=pre->path_cost+1;
+    curr->moves[curr->path_cost-1]=move;
+    curr->total_cost=curr->path_cost + huristic_manhattan(curr->grid);
+}
+
+node* Puzzle::select_best_node(){
+    node* curr=top;
+    int min=100000;
+    node* ret=NULL;
+    while(curr!=NULL){
+        if(curr->visited!=1 && curr->total_cost<min){
+            min=curr->total_cost;
+            ret=curr;
+        }
+        curr=curr->next;
+    }
+    if(ret!=NULL){
+        ret->visited=1;
+    }
+    return ret;
+}
+
 
 int Puzzle::huristic_manhattan(char current_grid[3][3]){
     int ret=0;
@@ -37,10 +106,13 @@ int Puzzle::huristic_manhattan(char current_grid[3][3]){
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
             temp=current_grid[i][j];
+            if(temp=='x'){
+                continue;
+            }
             int flag=0;
             for(k=0;k<3 && flag==0;k++){
                 for(l=0;l<3 && flag==0;l++){
-                  if(goal_grid[k][l]==current_grid[i][j]){
+                  if(goal_grid[k][l]==temp){
                       flag=1;
                   }
                 }
@@ -60,85 +132,67 @@ void Puzzle::swap(char& c1,char&c2){
     c2=temp;
 }
 
-char Puzzle::select_next_node(){
+void Puzzle::add_neighbours(node* temp){
     char temp_grid[3][3];
-    int h_value[4]={-1,-1,-1,-1};
     int pos_x=-1,pos_y=-1;
 
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-                temp_grid[i][j]=curr_grid[i][j];
+                temp_grid[i][j]=temp->grid[i][j];
         }
     }
 
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-            if(curr_grid[i][j]=='x'){
+            if(temp->grid[i][j]=='x'){
                 pos_x=i;
                 pos_y=j;
                 break;
             }
         }
     }
-    char temp;
     if(pos_y!=0){
         swap(temp_grid[pos_x][pos_y],temp_grid[pos_x][pos_y-1]);
-        h_value[0]=huristic_manhattan(temp_grid);
+        if(not_on_que(temp_grid)){
+            add_to_que(temp_grid,temp,'L');
+        }
         swap(temp_grid[pos_x][pos_y],temp_grid[pos_x][pos_y-1]);
     }
     if(pos_y!=2){
         swap(temp_grid[pos_x][pos_y],temp_grid[pos_x][pos_y+1]);
-        h_value[1]=huristic_manhattan(temp_grid);
+        if(not_on_que(temp_grid)){
+            add_to_que(temp_grid,temp,'R');
+        }
         swap(temp_grid[pos_x][pos_y],temp_grid[pos_x][pos_y+1]);
     }
     if(pos_x!=0){
         swap(temp_grid[pos_x][pos_y],temp_grid[pos_x-1][pos_y]);
-        h_value[2]=huristic_manhattan(temp_grid);
+        if(not_on_que(temp_grid)){
+            add_to_que(temp_grid,temp,'U');
+        }
         swap(temp_grid[pos_x][pos_y],temp_grid[pos_x-1][pos_y]);
     }
     if(pos_x!=2){
         swap(temp_grid[pos_x][pos_y],temp_grid[pos_x+1][pos_y]);
-        h_value[3]=huristic_manhattan(temp_grid);
+        if(not_on_que(temp_grid)){
+            add_to_que(temp_grid,temp,'D');
+        }
         swap(temp_grid[pos_x][pos_y],temp_grid[pos_x+1][pos_y]);
     }
-    int min=100;
-    int index=-1;
-    for(int i=0;i<4;i++){
-        if(h_value[i]>-1 && h_value[i]<min){
-            min=h_value[i];
-            index=i;
-        }
-    }
 
-    if(index==0){
-        swap(curr_grid[pos_x][pos_y],curr_grid[pos_x][pos_y-1]);
-        return 'L';
-    }
-    else if(index==1){
-        swap(curr_grid[pos_x][pos_y],curr_grid[pos_x][pos_y+1]);
-        return 'R';
-    }
-    else if(index==2){
-        swap(curr_grid[pos_x][pos_y],curr_grid[pos_x-1][pos_y]);
-        return 'U';
-    }
-    else if(index==3){
-        swap(curr_grid[pos_x][pos_y],curr_grid[pos_x+1][pos_y]);
-        return 'D';
-    }
-
-    return '-';
 }
 
 
 void Puzzle::get(){
     char temp;
+    node* new_node=new node();
+    top=new_node;
     cout<<"Enter initial state of grid"<<endl;
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
             cin>>temp;
             initial_grid[i][j]=temp;
-            curr_grid[i][j]=temp;
+            new_node->grid[i][j]=temp;
         }
     }
     cout<<"Enter goal state of grid"<<endl;
@@ -148,25 +202,35 @@ void Puzzle::get(){
             goal_grid[i][j]=temp;
         }
     }
+    new_node->path_cost=0;
+    new_node->total_cost=huristic_manhattan(new_node->grid);
 }
 
 void Puzzle::solve(){
-    int hur=-1;
 
-    while(hur!=0){
-        moves[no_of_steps++]=select_next_node();
-        cout<<moves[no_of_steps-1]<<"\t";
-        hur=huristic_manhattan(curr_grid);
+    while(1){
+        node* temp=select_best_node();
+        if(temp==NULL){
+            cout<<"No solution to the problem"<<endl;
+            break;
+        }
+        if(huristic_manhattan(temp->grid)==0){
+            print_result(temp);
+        }
+        else{
+            add_neighbours(temp);
+        }
+
     }
 }
 
-void Puzzle::print_result(){
+void Puzzle::print_result(node* res){
     char ch;
     cout<<"Initial state"<<endl;
     print_grid(initial_grid);
     cout<<"Moves taken"<<endl;
-    for(int i=0;i<no_of_steps;i++){
-        cout<<moves[i]<<"\t";
+    for(int i=0;i< res->path_cost;i++){
+        cout<<res->moves[i]<<"\t";
     }
     cout<<endl;
     cout<<"final state"<<endl;
@@ -174,7 +238,7 @@ void Puzzle::print_result(){
     cout<<"Print steps(Y/N)?"<<endl;
     cin>>ch;
     if(ch=='y' || ch=='Y'){
-        print_steps();
+        print_steps(res);
     }
 }
 
@@ -187,7 +251,7 @@ void Puzzle::print_grid(char g[3][3]){
     }
 }
 
-void Puzzle::print_steps(){
+void Puzzle::print_steps(node* res){
     char temp[3][3];
     int x=-1,y=-1;
     for(int i=0;i<3;i++){
@@ -199,8 +263,8 @@ void Puzzle::print_steps(){
             }
         }
     }
-    for(int i=0;i<no_of_steps;i++){
-        switch(moves[i]){
+    for(int i=0;i<res->path_cost;i++){
+        switch(res->moves[i]){
             case 'L':
                 cout<<"\nL"<<endl;
                 swap(temp[x][y],temp[x][--y]);
@@ -229,6 +293,5 @@ int main() {
     Puzzle obj;
     obj.get();
     obj.solve();
-    obj.print_result();
     return 0;
 }
